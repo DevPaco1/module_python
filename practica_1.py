a86b0d45cdefbb872286eb2f01c2671630419c42
PaquitoRivera <paco.dev.mexico@gmail.com>
2023-06-30 16:49:22 -0600

    Finish practica_1

--- practica_1.py
+++ practica_1.py
@@ -1,4 +1,18 @@
-'''#exercise 1
+'''1. Escribe un script que imprima las primeras estrofas de la canción de la víbora de la mar con el siguiente formato:
+
+A la víbora, víbora
+	De la mar, de la mar
+		Por aquí pueden pasar
+	Los de adelante corren mucho
+		Y los de atrás se quedarán
+
+A la víbora, víbora
+	De la mar, de la mar
+		Por aquí pueden pasar
+	Los de adelante corren mucho
+		Y los de atrás se quedarán
+		Tras, tras, tras'''
+
 line_1 ='A la vibora, vibora\n'
 line_2 ='De la mar, de la mar\n'
 line_3 ='Por aqui pueden pasar\n'
@@ -12,9 +26,9 @@ print(line_1,two_spaces + line_2,four_spaces + line_3,two_spaces + line_4,four_s
 print(line_1,two_spaces + line_2,four_spaces + line_3,two_spaces + line_4,four_spaces + line_5,four_spaces + line_6)
 
 
-#exercise 2
-print('\n')
-
+'''2. Escribe un programa que solicite el nombre, el apellido y 
+la edad del usuario e imprima un saludo y su año de nacimiento. 
+Tomar el año actual como constante (2023)'''
 
 nombre = input('Escibe tu primer nombre: ')
 apellido = input('Escribe tu primer apellido: ')
@@ -23,8 +37,11 @@ calculo_edad = str(edad-2023)
 
 print('Hola',nombre,apellido + ',','naciste en',calculo_edad[-4:])
 
-#Exercise 3
-print('\n')
+'''
+3. Escribe un script que calcule el área de un círculo 
+basado en el radio proporcionado por el usuario e imprima el resultado.
+'''
+
 
 radio = int(input('Escribe el radio de un circulo: '))
 pi = 3.1416
@@ -32,13 +49,14 @@ calculo_area = pi * (radio**2)
 
 print('El ares del circulo es:',calculo_area)
 
-#Exercise 4
-print('\n')
+'''4. Escribe un script que solicite un número (n) y calcule el valor de n + nn + nnn'''
+
 
 numero = input('Escribe un numero: ')
-print('la suma de:',numero,"+",numero*2,'+',numero*3,'=',int(numero)+int(numero*2)+int(numero*3))
+print('la suma de:',numero,"+",numero,'+',numero,'=',int(numero)+int(numero**2)+int(numero**3))
 
-#Exercise 5'''
+'''5. Escribe un script que solicite un número y devuelva notifique 
+ usuario si el número es par o impar'''
 
 
 print('Escribe un numero')
@@ -48,30 +66,13 @@ if numero == input(float()):
                 print(numero,'es par')
         else :
                 print(numero,'es impar')
-else:
-    print('Escribe un digito')
 
-while numero != int():
-    print('escribe un digito')
-    numero = input(int())
-    print('gracias')
        
 
-'''print('what is your age?')
-myAge = input(int())
- 
-if myAge == input(int()):
-    print('thank you')
-else:
-    print('please enter the number only')
- 
-while myAge != int():
-    print('please enter the number only')
-    myAge = input(int())
-print('thank you')'''
-'''#Exercise 6
-print('\n')
+'''6. Escribe un script que solicite la base y la altura de un triángulo,
+ calcule su área e imprima el resultado'''
+
 
 base = int(input('Escribe la base del triangulo: '))
-altura = int(input('Escribe la altura del triangulo: '))'''
-
+altura = int(input('Escribe la altura del triangulo: '))
+print(f'El area del triangulo es: {base*altura/2}')
